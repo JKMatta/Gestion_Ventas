c10bfb889cfe1197411ef5df5348c61f9bd54e9e
Cambios en General <javier.alexander335@outlook.com>
2018-11-01 22:09:05 -0300

    Actualizacion de la Database

--- DataBase/bd.sql
+++ DataBase/bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-11-2018 a las 19:17:21
+-- Tiempo de generación: 02-11-2018 a las 02:08:21
 -- Versión del servidor: 10.1.36-MariaDB
 -- Versión de PHP: 7.2.11
 
@@ -43,7 +43,52 @@ CREATE TABLE `game` (
 INSERT INTO `game` (`serial`, `nombre`, `descripcion`, `restriccion`, `valor`) VALUES
 (5545, 'rocket_league', 'jugador competitivo', 'T', 7500),
 (7815, 'Fallout_4', 'El mundo despues de la guerra', 'M', 6400),
-(1245, 'Outlast_2', 'la maldicion del viejo', 'M', 4500);
+(1245, 'Outlast_2', 'la maldicion del viejo', 'M', 4500),
+(3245, 'TestLink', 'This is a test', 'E', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `id` int(11) NOT NULL,
+  `correo` varchar(120) NOT NULL,
+  `juego` varchar(80) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `venta`
+--
+
+INSERT INTO `venta` (`id`, `correo`, `juego`, `cantidad`, `total`) VALUES
+(1, 'asd', 'rocket_league', 2, 15000),
+(109, 'javier.alexander335@outlook.com', 'rocket_league', 1, 7500),
+(207, 'javier.alexander335@outlook.com', 'rocket_league', 1, 7500),
+(492, 'asdd', 'Fallout_4', 1, 6400);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=493;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
